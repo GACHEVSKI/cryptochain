3415d0ba439a77ebd3b9f534fde3ef62b5ea5da0
Gachevski <hektodaleky@gmail.com>
2022-11-30 11:54:58 +0600

    redis and messages listeners

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const Blockchain = require("./blockchain");
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubSub = new PubSub({blockchain});
 
-app.get('/api/blockchain', (req, res) => {
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_REQUEST = `http://localhost:${DEFAULT_PORT}`;
+setTimeout(() => pubSub.broadcastChain(), 1000);
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
-const PORT = 3000;
+app.post('/api/mine', (req, res) => {
+    const {data} = req.body;
+    blockchain.addBlock({data});
+    pubSub.broadcastChain();
 
+    res.redirect('/api/blocks');
+});
+
+const syncChains = () => {
+    axios.get(`${ROOT_NODE_REQUEST}/api/blocks`).then(res => {
+        blockchain.replaceChain(res.data)
+    }, err => {
+        console.error(err)
+    })
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT) {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
    console.log(`Listening on ${PORT} port`);
+    syncChains();
 });
